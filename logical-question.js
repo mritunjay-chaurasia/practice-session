215b91a2a2ca22476cd5d9c659ecc6a100654fd7
Mritunjay Chaurasia <mritunjayc004@gmail.com>
2025-09-28 22:26:16 +0530

    add

---
+++ logical-question.js
@@ -0,0 +1,186 @@
+//Q.1 Find non repeat string
+//     const data = SAwiSS
+
+//Q.2  
+// 1
+// 1 35
+// 1 35 17
+
+//Q.3 Print numbers in descending order per row:
+// 54321
+// 4321
+// 321
+// 21
+// 1
+
+// function printPattern(n){
+//   for(let i = n; i > 0; i--){
+//     let row = ""
+//     for(let j = i; j > 0 ; j--){
+//         row += j
+//     }
+//     console.log(row)
+//   }
+// }
+// printPattern(5)
+
+//Q.4 Print numbers in ascending order per row:
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+
+//Q.5  
+// 12345
+// 2345
+// 345
+// 45
+// 5
+
+// function printPattern(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let row = ""
+//         for (let j = i; j <= n; j++) {
+//             row += j
+//         }
+//         console.log(row)
+//     }
+// }
+
+// printPattern(5)
+
+//Q.6 Right-angled triangle with numbers:
+//     1
+//    12
+//   123
+//  1234
+// 12345
+
+//Q.7 Inverted right-angled triangle with numbers:
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+//Q.8 Floyd’s Triangle:
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+
+//Q.9 Pyramid with numbers:
+//     1
+//    121
+//   12321
+//  1234321
+
+
+
+//Q.10 Diamond number pattern:
+
+//     1
+//    121
+//   12321
+//  1234321
+//   12321
+//    121
+//     1
+
+//Q.11 Square pattern:
+// *****
+// *****
+// *****
+// *****
+
+//Q.12 Right-angled triangle:
+// *
+// **
+// ***
+// ****
+// *****
+
+//Q.13 Inverted triangle:
+// *****
+// ****
+// ***
+// **
+// *
+
+
+//Q.14 Pyramid
+
+//     *
+//    ***
+//   *****
+//  *******
+
+//Q.15 Inverted pyramid:
+//  *******
+//   *****
+//    ***
+//     *
+
+//Q.16 Diamond
+//     *
+//    ***
+//   *****
+//  *******
+//   *****
+//    ***
+//     *
+
+//Q.17 Hollow square:
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+//Q.18 Hollow pyramid:
+
+//     *
+//    * *
+//   *   *
+//  *******
+
+//Q.19 Pascal’s Triangle (binomial coefficients):
+//     1
+//    1 1
+//   1 2 1
+//  1 3 3 1
+
+//Q.20 Butterfly pattern:
+// *      *
+// **    **
+// ***  ***
+// ********
+// ***  ***
+// **    **
+// *      *
+
+//Q.21 Hourglass pattern:
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+//Q.22 Checkerboard pattern:
+// * * * * *
+//  * * * *
+// * * * * *
+//  * * * *
+
+
+//Q.23 0-1 Triangle:
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
